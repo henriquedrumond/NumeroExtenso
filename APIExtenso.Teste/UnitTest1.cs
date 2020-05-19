36afa20b65508b20151b17391be4a465ea63cee0
Henrique Queiroz Drumond <henriquedrumond@hotmail.com>
2020-05-19 10:14:51 -0300

    Adicionado projeto de Teste.

---
+++ APIExtenso.Teste/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using System;
+using ExecucaoNumero.Conversao;
+using APIExtenso.Helpers;
+
+namespace APIExtenso.Teste
+{
+    [TestFixture]
+    public class TesteDeInteiroInterno
+    {
+        [TestCase("um", 1)]
+        [TestCase("menos mil e quarenta e dois", -1042)]
+        [TestCase("noventa e quatro mil, quinhentos e oitenta e sete", 94587)]
+
+        public void DeveEscreverSemConjuncaoQuandoAlgumAlgarismoForZero(string numeroPorExtensoEsperado, int numero)
+        {
+            Assert.AreEqual(numeroPorExtensoEsperado, numero.PorExtenso());
+        }
+    }
+}
---
+++ APIExtenso/Helpers/ExtensoesDeNumerosInteiros.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ExecucaoNumero.Conversao;
+
+namespace APIExtenso.Helpers
+{
+    public static class ExtensoesDeNumerosInteirosCoreTest
+    {
+        public static string PorExtenso(this int valor)
+        {
+            var numeroPorExtenso = ConversaoNumeros.Converter(valor).ToString();
+
+            string _nunmeronegativo = valor.ToString();
+
+            numeroPorExtenso = _nunmeronegativo.Contains('-') ? "menos " + numeroPorExtenso : numeroPorExtenso;
+
+            return numeroPorExtenso;
+        }
+    }
+}
